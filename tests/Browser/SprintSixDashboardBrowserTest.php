6e7264a253299d53e2e9889ebdf2e625dd8b4852
Ruben T <ruby@Rubens-MacBook-Pro.local>
2026-03-09 21:03:49 -0600

    Enable Pest browser tests

--- tests/Browser/SprintSixDashboardBrowserTest.php
+++ tests/Browser/SprintSixDashboardBrowserTest.php
@@ -7,18 +7,13 @@ use Tests\TestCase;
 uses(TestCase::class, RefreshDatabase::class);
 
 it('renders sprint 6 dashboard panels without javascript errors', function () {
-    $visitFunction = 'Pest\\Browser\\visit';
-    if (! function_exists($visitFunction)) {
-        $this->markTestSkipped('pest-plugin-browser is not installed in this environment.');
-    }
-
     config()->set('services.donations.donation_url', 'https://ko-fi.com/poofmq');
 
     $user = User::factory()->create();
 
     $this->actingAs($user);
 
-    $visitFunction('/dashboard')
+    visit('/dashboard')
         ->assertSee('Funding overview')
         ->assertSee('Net funding')
         ->assertSee('Support me on Ko-fi')
